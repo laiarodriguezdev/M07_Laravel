50f442b2f24b9bc8e6a389e437ea10b93442a765
Laia Rodriguez Ramos <lrodrigu22@jaumebalmes.net>
2023-12-14 16:43:01 +0100

    Model i primera migració feta.

---
+++ database/migrations/2023_12_14_153842_nom_migracion.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('NomMigracion', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('nom');
+            $table->string('cognoms');
+            $table->string('password');
+            $table->string('email')->unique();
+            $table->string('rol');
+            $table->boolean('actiu');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('NomMigracion');
+    }
+};
