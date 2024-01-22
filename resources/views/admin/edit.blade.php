dab1a7cbd60f5c4d2d89759c90510374b5b19f82
Laia Rodriguez Ramos <lrodrigu22@jaumebalmes.net>
2024-01-22 19:39:53 +0100

    Editar intentant-ho.

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -14,16 +14,49 @@
 
     <h1>Editar Profesor</h1>
 
+    <form action="{{ url('/admin/edit') }}" method="put">
+        <!--ID-->
+        <label for="id"> Numero</label>
+        <input type="number" name="id">
+        <br>
+        <!--NOM-->
+        <label for="nom"> Nom</label>
+        <input type="text" name="nom">
+        <br>
+        <!--COGNOM-->
+        <label for="cognoms"> Cognoms </label>
+        <input type="text" name="cognoms">
+        <br>
+        <!--PASSWORD-->
+        <label for="password"> Contrassenya </label>
+        <input type="text" name="password">
+        <br>
+        <!--EMAIL-->
+        <label for="email"> Email </label>
+        <input type="text" name="email">
+        <br>
+        <!--ROL-->
+        <label for="rol"> Rol </label>
+        <select name="rol">
+            <option value="Alumne">Alumne</option>
+        </select>
+        <br>
+        <!--ACTIU-->
+        <label for="actiu"> Actiu? </label>
+        <input type="checkbox" name="actiu">
+        <br>
+        <input type="submit" value="Enviar">   
+    </form>
+
+    <!--
     <form action="{{ route('admin.update', $professor->id) }}" method="post">
         @csrf
         @method('put')
 
-        <!-- Campos del formulario -->
-        <!-- Puedes cargar los valores actuales del profesor en los campos del formulario -->
 
         <button type="submit">Guardar Cambios</button>
     </form>
-
+    -->
     <br>
     <a href="{{ route('admin.index')}}">Torna a home</a>
 </body>
